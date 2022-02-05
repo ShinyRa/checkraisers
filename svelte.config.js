9228e72bc3124f6b057bcf36fb61fffffd0a7941
tijs <tijs.de.graaff@hva.nl>
2022-02-05 20:46:07 +0100

    Added base path

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,8 @@ const config = {
 		/**
 		 * Adapter config for Github / Gitlab pages
 		 */
-		// adapter: adapter({ pages: 'build', assets: 'build', fallback: null })
-		adapter: adapter()
+		adapter: adapter({ pages: 'build', assets: 'build', fallback: null }),
+		paths: { base: '/pokerapp' }
 	}
 };
 
