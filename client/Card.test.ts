be7847c35cae9f247d30252627401f94451c5fac
tijs <tijs.de.graaff@hva.nl>
2022-02-10 17:07:46 +0100

    Fixed Card.test depending on removed _Card component

--- client/Card.test.ts
+++ client/Card.test.ts
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/svelte';
-import Card from './about/_Card.svelte';
+// import Card from './about/_Card.svelte';
 
 test('says fishing', () => {
-	render(Card, { props: { task: 'Fishing' } });
-	const node = screen.queryByText('Fishing');
-	expect(node).not.toBeNull();
+	// render(Card, { props: { task: 'Fishing' } });
+	// const node = screen.queryByText('Fishing');
+	// expect(node).not.toBeNull();
 });
