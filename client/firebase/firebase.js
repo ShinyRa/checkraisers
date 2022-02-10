78edd59c1a44997f0cafc88e094645e30cc18162
steenma1 <Auke.steenman@gmail.com>
2022-02-10 17:13:59 +0100

    removed firebase usage for public use

--- client/firebase/firebase.js
+++ client/firebase/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 //dotenv.config()
 const firebaseConfig = {
-	apiKey: 'APIKEY',
-	authDomain: 'pokerapp-a4302.firebaseapp.com',
-	projectId: 'pokerapp-a4302',
-	storageBucket: 'pokerapp-a4302.appspot.com',
-	messagingSenderId: '704885010595',
-	appId: '1:704885010595:web:22997e4a68935174cd4f16'
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: ''
 };
 
 // Initialize Firebase
