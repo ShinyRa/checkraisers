fb1fa3f4c88c85b68789fba0f24d067bc1ac39e6
tijs <tijs.de.graaff@hva.nl>
2022-02-12 13:54:43 +0100

    Begone 1px card border 🙏

--- frontend/routes/card/_PlayingCard.svelte
+++ frontend/routes/card/_PlayingCard.svelte
@@ -16,15 +16,20 @@
 
 	let suitName = CardSuit[card.suit].toLowerCase();
 	let valueName = CardValue[card.value].toLowerCase();
+
+	const reveal = () => {
+		faceUp = !faceUp;
+		known = true;
+	};
 </script>
 
 <div
-	class="playingcard"
-	on:click={() => ((faceUp = !faceUp), (known = true))}
+	class="card playingcard"
+	on:click={reveal}
 	class:faceup={faceUp}
-	in:fly={{ y: 50, duration: 325 }}
+	in:fly={{ y: -40, x: 8, duration: 250 }}
 >
-	<img src="{assets}/alien.gif" alt="Cardback" class="face" />
+	<img src="{assets}/cards/cardback.png" alt="Cardback" class="face" />
 	{#if known}
 		<img
 			src="{assets}/cards/{valueName}_of_{suitName}.png"
@@ -35,38 +40,41 @@
 </div>
 
 <style lang="scss">
-	.faceup {
-		transform: rotateY(180deg);
-	}
-
-	img {
-		pointer-events: none;
-		-moz-user-select: none;
-		-webkit-user-select: none;
-		user-select: none;
-		height: 100%;
-		width: 100%;
-	}
-
 	.playingcard {
-		height: 280px;
-		width: 190px;
-		border-radius: 25px;
+		height: 268px;
+		width: 185px;
+		border-radius: 8px;
 		transform-style: preserve-3d;
-		transition: transform 0.25s ease-out;
+		transition: transform 0.25s linear;
+		position: relative;
 
 		.face {
 			position: absolute;
 			-webkit-backface-visibility: hidden;
 			backface-visibility: hidden;
+			background-color: transparent;
 		}
 
 		.front {
 			transform: rotateY(180deg);
+			outline: 3px solid white;
+			outline-offset: -2px;
 		}
 	}
-
+	img {
+		height: 100%;
+		width: 100%;
+		border-radius: 8px;
+		pointer-events: none;
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		user-select: none;
+	}
 	.playingcard:hover {
 		cursor: pointer;
+	}
+
+	.faceup {
+		transform: rotateY(180deg);
 	}
 </style>
