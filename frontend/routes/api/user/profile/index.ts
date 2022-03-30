7e4f07a418f9855463b77dfcde0400c5f328405d
steenma1 <Auke.steenman@gmail.com>
2022-03-30 18:43:28 +0200

    some small refactoring

--- frontend/routes/api/user/profile/index.ts
+++ frontend/routes/api/user/profile/index.ts
@@ -1,13 +1,13 @@
 import UserAPI from '$lib/api/user';
 
-const userAPI = new UserAPI('users');
+const userAPI = new UserAPI();
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function post({ request }) {
 	return await userAPI.updateProfile(await request.json());
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function get({ params }) {
-	return await userAPI.getProfile(await params.email);
-}
+// not needed atm
+// export async function get({ params }) {
+// 	return await userAPI.getProfile(await params.email);
+// }
--- frontend/routes/api/user/register/index.ts
+++ frontend/routes/api/user/register/index.ts
@@ -1,6 +1,6 @@
 import UserAPI from '$lib/api/user';
 
-const userAPI = new UserAPI('users');
+const userAPI = new UserAPI();
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function post({ request }) {
