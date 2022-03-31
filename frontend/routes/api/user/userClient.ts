6deb254c2a4fe591c26f41a61f426895405d1358
Auke Steenman <auke.steenman@hva.nl>
2022-03-31 09:49:12 +0200

    client refactor

--- frontend/routes/api/user/userClient.ts
+++ frontend/routes/api/user/userClient.ts
@@ -1,39 +1,30 @@
 import { User } from '$lib/entities/user/User';
 
-const requestOptions = (user: Partial<User>) => {
-	return {
+
+const httpRequest = async(url: string, data: Partial<User>): Promise<unknown> => {
+	const requestOptions = {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-		body: JSON.stringify(user)
+		body: JSON.stringify(data)
 	};
-};
+
+	return await fetch(url, requestOptions)
+	.then((resp) => {
+		return resp.json();
+	})
+	.then((json) => {
+		return json;
+	});
+}
 
 export const userClient = {
 	login: async (user: Partial<User>): Promise<unknown> => {
-		return await fetch(`api/user/login`, requestOptions(user))
-			.then((resp) => {
-				return resp.json();
-			})
-			.then((json) => {
-				return json;
-			});
+		return httpRequest('api/user/login',user)
 	},
 	register: async (user: User): Promise<unknown> => {
-		return await fetch(`api/user/register`, requestOptions(user))
-			.then((resp) => {
-				return resp.json();
-			})
-			.then((json) => {
-				return json;
-			});
+		return httpRequest('api/user/register',user)
 	},
 	update: async (user: Partial<User>): Promise<unknown> => {
-		return await fetch(`api/user/profile`, requestOptions(user))
-			.then((resp) => {
-				return resp.json();
-			})
-			.then((json) => {
-				return json;
-			});
+		return httpRequest('api/user/profile',user)
 	}
 };
