78c1a11b456cac246629fa4ca2470065e62d973c
Auke Steenman <auke.steenman@hva.nl>
2022-04-10 22:45:19 +0200

    Creating tests

--- backend/api/user/index.ts
+++ backend/api/user/index.ts
@@ -1,24 +1,23 @@
 import { HttpCode } from '../../utils/HttpStatusCode';
 import BaseAPI, { type Response } from './../BaseAPI';
 import { type User } from './../../entities/user/User';
-import { Collection } from 'mongodb';
 
 class UserAPI extends BaseAPI {
 	private PROFILE_PICTURE_PATH = './../images/profile_pictures/';
-	private db: Collection;
 
 	constructor() {
 		super();
-		this.getCollection('users').then((res) => {
-			this.db = res;
-		});
 	}
 
 	public getProfile = async (email: User['email']): Promise<Response> => {
 		try {
-			const user = await this.db.findOne({ email: email }).then((result) => {
+			await this.openDbConnection();
+			const db = await this.getCollection('users');
+			const user = await db.findOne({ email: email }).then((result) => {
 				return result ? result : false;
 			});
+			await this.closeDbConnection();
+
 			return user
 				? this.httpResponse(HttpCode.SUCCESS, user)
 				: this.httpResponse(HttpCode.NOT_FOUND, { error: 'No user found with: ' + email });
@@ -28,13 +27,33 @@ class UserAPI extends BaseAPI {
 		}
 	};
 
+	public removeUser = async (email: User['email']): Promise<Response> => {
+		try {
+			await this.openDbConnection();
+			const db = await this.getCollection('users');
+			const res = await db.deleteOne({ email: email });
+			await this.closeDbConnection();
+			return res.acknowledged
+				? this.httpResponse(HttpCode.SUCCESS, { success: 'Deleted the user' })
+				: this.httpResponse(HttpCode.BAD_REQUEST, {
+						error: 'Something went wrong with deleting a user'
+				  });
+		} catch (err) {
+			console.log('try catch error Register:\n', err);
+			return this.httpResponse(HttpCode.SERVER_ERROR, { error: 'User could not be deleted' });
+		}
+	};
+
 	public updateProfile = async (data: FormData): Promise<Response> => {
 		try {
 			let user: User;
 			for (const pair of data.entries()) {
 				user = { ...user, ...{ [pair[0]]: pair[1] } };
 			}
-			const userPresent = await this.db.findOne({ email: user.email }).then((result) => {
+
+			await this.openDbConnection();
+			const db = await this.getCollection('users');
+			const userPresent = await db.findOne({ email: user.email }).then((result) => {
 				return result ? true : false;
 			});
 
@@ -47,11 +66,12 @@ class UserAPI extends BaseAPI {
 					user.profilePicture = path;
 				}
 
-				const res = await this.db.findOneAndUpdate(
+				const res = await db.findOneAndUpdate(
 					{ email: user.email },
 					{ $set: user },
 					{ upsert: false, returnDocument: 'after', projection: { _id: 0, password: 0 } }
 				);
+				await this.closeDbConnection();
 
 				if (user.profilePicture)
 					res.value['profilePicture'] = this.readFromDisk(res.value['profilePicture']);
@@ -72,14 +92,17 @@ class UserAPI extends BaseAPI {
 	public register = async (user: User): Promise<Response> => {
 		const DEFAULT_CHIP_AMOUNT = 1000;
 		try {
-			const userPresent = await this.db.findOne({ email: user.email }).then((result) => {
+			await this.openDbConnection();
+			const db = await this.getCollection('users');
+			const userPresent = await db.findOne({ email: user.email }).then((result) => {
 				return result ? true : false;
 			});
 			if (!userPresent) {
 				user.password = this.hash(user.password);
 				user.chips = DEFAULT_CHIP_AMOUNT;
 				user.profilePicture = `${this.PROFILE_PICTURE_PATH}default.png`;
-				const res = await this.db.insertOne(user);
+				const res = await db.insertOne(user);
+				await this.closeDbConnection();
 				return res.acknowledged
 					? this.httpResponse(HttpCode.SUCCESS, { success: 'Created new user' })
 					: this.httpResponse(HttpCode.BAD_REQUEST, { error: 'Could not create User' });
@@ -94,7 +117,9 @@ class UserAPI extends BaseAPI {
 
 	public login = async (user: Record<User['email'], User['password']>): Promise<Response> => {
 		try {
-			const profile = await this.db
+			await this.openDbConnection();
+			const db = await this.getCollection('users');
+			const profile = await db
 				.findOne(
 					{ email: user.email, password: this.hash(user.password) },
 					{ projection: { _id: 0, password: 0 } }
@@ -102,7 +127,7 @@ class UserAPI extends BaseAPI {
 				.then((result) => {
 					return result ? result : false;
 				});
-
+			await this.closeDbConnection();
 			if (profile) profile['profilePicture'] = this.readFromDisk(profile['profilePicture']);
 			return profile
 				? this.httpResponse(HttpCode.SUCCESS, profile)
---
+++ backend/testing/database/Crud.test.ts
@@ -0,0 +1,63 @@
+import 'regenerator-runtime/runtime';
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+import UserAPI from '../../api/user';
+import { type User } from '../../entities/user/User';
+
+let user: User;
+let userAPI: UserAPI;
+
+const testUser: User = {
+	email: 'testuser@email.nl',
+	username: 'username',
+	password: 'password'
+};
+
+beforeAll(() => {
+	userAPI = new UserAPI();
+	user = testUser;
+});
+
+describe('user operations on database', () => {
+	it('should register a new user', async () => {
+		const expectedResult = { success: 'Created new user' };
+		const result = await userAPI.register(user);
+		expect(result.body).toEqual(expectedResult);
+	});
+
+	it('should login a user', async () => {
+		const result = await userAPI.login({ email: user.email, password: user.password });
+
+		//we have to wait some seconds extra with a timeout for the response to come back. even if you use await.
+		//Jest still prematurely does the toEqual check.
+		setTimeout(() => {
+			expect(result.body['email']).toEqual(user.email);
+		}, 2000);
+	});
+
+	it('should get a user profile', async () => {
+		const result = await userAPI.getProfile(user.email);
+		expect(result.body['email']).toEqual(user.email);
+	});
+
+	it('should update a user', async () => {
+		user.username = 'updatedUserName';
+		user.chips = 1234567;
+		const result = await userAPI.update({
+			email: user.email,
+			username: user.username,
+			chips: user.chips
+		});
+		setTimeout(() => {
+			expect(result.body['email']).toEqual(user.email);
+		}, 2000);
+	});
+
+	it('should delete a user', async () => {
+		const expectedResult = { success: 'Deleted the user' };
+		const result = await userAPI.removeUser(user.email);
+		expect(result.body).toEqual(expectedResult);
+	});
+});
