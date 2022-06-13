0f192d808879db1a9d0d571691de23c952493878
Auke Steenman <auke.steenman@hva.nl>
2022-06-13 03:04:13 +0200

    Added global socket URL

--- lib/logic/frontend/entities/stores/index.ts
+++ lib/logic/frontend/entities/stores/index.ts
@@ -2,5 +2,5 @@ import { io, Socket } from 'socket.io-client';
 import { readable, writable, type Readable } from 'svelte/store';
 import UserRepository from '../repositories/UserRepository';
 
-export const userStore = writable(new UserRepository())
-export const socketStore: Readable<Socket> = readable(io('ws://localhost:3001'))
+export const userStore = writable(new UserRepository());
+export const socketStore: Readable<Socket> = readable(io('ws://checkraisers.nl:3001'));
